b0b7122394327b2e774ef77258821030f5d9103e
karl wernsman <wernsmank@gmail.com>
2023-01-11 14:47:49 -0800

    title state working

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,16 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import Preview from '../Preview/Preview';
 import Editor from '../Editor/Editor';
-
 import './Home.css';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
-
+  const [title, setTitle] = useState('title');
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview title={title} />
+      <Editor {...{ title, setTitle }} />
     </main>
   );
 }
